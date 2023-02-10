02a085b3f9f2f2e3311f041f679d8b6f1d02f710
Sergio Nicolas Fernandez <sergionicolasfernandez2002@gmail.com>
2023-02-10 17:49:48 -0300

    2da entrega

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-
 let alumnoUsuario = "coder"
 let passwordUsuario = "coder"
+let intentos = 0
+const maxIntentos = 3
 let usuarioIngresado = prompt("Ingrese su usuario")
 let passwordIngresado = prompt("Ingrese su contraseña")
 if (usuarioIngresado === alumnoUsuario && passwordIngresado === passwordUsuario) {
@@ -8,11 +9,12 @@ if (usuarioIngresado === alumnoUsuario && passwordIngresado === passwordUsuario)
 } else {
     alert("No puede ingresar, los datos son invalidos")
 } while (usuarioIngresado != alumnoUsuario && passwordIngresado != passwordUsuario) {
-    let alumnoUsuario = "coder"
-    let passwordUsuario = "coder"
-    let usuarioIngresado = prompt("Ingrese su usuario")
-    let passwordIngresado = prompt("Ingrese su contraseña")
+    alumnoUsuario = "coder"
+    passwordUsuario = "coder"
+    usuarioIngresado = prompt("Ingrese su usuario")
+    passwordIngresado = prompt("Ingrese su contraseña")
 }
+
 
 alert("Bienvenidos alumnos a la pagina del Instituto.");
 alert("Tendran que ingresar sus datos personales e incluso todas sus notas, para evaluarles si lograron aprobar el curso asignado.");
@@ -20,7 +22,7 @@ alert("Si reprueba el curso, tendrá que volver a entregar o volver a cursar en 
 
 let nombreAlumno = prompt("ingrese a la pagina su Nombre");
 let apellidoAlumno = prompt("ingrese su Apellido");
-console.log ("Nombre y Apellido del alumno")
+console.log("Nombre y Apellido del alumno")
 console.log(nombreAlumno);
 console.log(apellidoAlumno);
 
@@ -46,13 +48,13 @@ if (promedioFinal >= 6) {
     alert("Aprobaste el curso, Felicidades");
 } else if (promedioFinal >= 4.5 && promedioFinal <= 5.9) {
     alert("Tenes que realizar otra entrega para aprobar el curso");
-    let recuperatorioEntrega = parseInt (prompt("Ingrese la nota del recuperatorio"))
-    if (recuperatorioEntrega >= 6){
-    alert ("Aprobaste el recuperatorio, Felicidades")
-    console.log ("La nota del recuperatorio es:")
-    console.log (recuperatorioEntrega)
-    } else if  (recuperatorioEntrega <= 5.9){
-        alert ("Desaprobaste el recuperatorio, volve a recursar")
+    let recuperatorioEntrega = parseInt(prompt("Ingrese la nota del recuperatorio"))
+    if (recuperatorioEntrega >= 6) {
+        alert("Aprobaste el recuperatorio, Felicidades")
+        console.log("La nota del recuperatorio es:")
+        console.log(recuperatorioEntrega)
+    } else if (recuperatorioEntrega <= 5.9) {
+        alert("Desaprobaste el recuperatorio, volve a recursar")
         console.log("La nota del recuperatorio es:")
         console.log(recuperatorioEntrega)
     }
@@ -60,3 +62,57 @@ if (promedioFinal >= 6) {
     (promedioFinal >= 0 && promedioFinal >= 4.4);
     alert("Reprobaste el curso,volve a intentarlo en el proximo");
 }
+class alumno {
+    constructor(nombre, apellido, promedio) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.promedio = promedio;
+    }
+}
+const alumno0 = new alumno("Lucas", "Rob", 9);
+const alumno1 = new alumno("Maria", "Palechii", 7.75)
+const alumno2 = new alumno(nombreAlumno, apellidoAlumno, promedioFinal)
+const alumno3 = new alumno("Matias", "Hazard", 6.75)
+
+console.log("Las notas de los Alumnos son:")
+console.log(alumno0)
+console.log(alumno1)
+console.log(alumno2)
+console.log(alumno3)
+
+alumnos = [alumno0, alumno1, alumno2, alumno3]
+notaAlta = Math.max(...alumnos.map(alumno => alumno.promedio))
+const alumnoNotaAlta = alumnos.find(alumno => alumno.promedio === notaAlta)
+notaBaja = Math.min(...alumnos.map(alumno => alumno.promedio))
+const alumnoNotaBaja = alumnos.find(alumno => alumno.promedio === notaBaja)
+console.log("El alumno con la nota mas alta es: " + alumnoNotaAlta.nombre, alumnoNotaAlta.apellido)
+console.log(`Su nota mas alta fue: ${notaAlta}`)
+console.log("El alumno con la nota mas alta es: " + alumnoNotaBaja.nombre, alumnoNotaBaja.apellido)
+console.log(`Su nota mas baja fue: ${notaBaja}`)
+
+const cuotaAlumno = prompt("Ingrese |Pagado| o |Deuda| dependiendo si pago o no la cuota.")
+console.log("La cuota del alumno ", nombreAlumno, "", apellidoAlumno, " esta en estado: ", cuotaAlumno)
+let cuota = []
+cuota.push(alumno0.apellido + "." + "Pagado")
+cuota.push(alumno1.apellido + "." + "Pagado")
+cuota.push(alumno2.apellido + "." + cuotaAlumno)
+cuota.push(alumno3.apellido + "." + "Deuda")
+console.log(cuota)
+
+class Deuda {
+    constructor(nombre, apellido, cuota) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.cuota = cuota;
+    }
+}
+
+const deudas = [
+    new Deuda(alumno0.nombre, alumno0.apellido, "Pagado"),
+    new Deuda(alumno1.nombre, alumno1.apellido, "Pagado"),
+    new Deuda(alumno2.nombre, alumno2.apellido, cuotaAlumno),
+    new Deuda(alumno3.nombre, alumno3.apellido, "Deuda")
+];
+
+const deudasPendientes = deudas.filter(deuda => deuda.cuota === "Deuda");
+console.log(deudasPendientes);
